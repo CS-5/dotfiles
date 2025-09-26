e52c920f556d6727b761ae052ac15e61ccb168b0
Carson Seese <carson@carsonseese.com>
2025-09-25 20:19:31 -0400

    fix the fishn

--- root/private_dot_config/fish/functions/fzkill.fish
+++ root/private_dot_config/fish/functions/fzkill.fish
@@ -1,3 +1,6 @@
 function fzkill
-  ps -eo pid,args,%mem,%cpu | fzf --multi --header-lines=1 | awk '{print $1}' | xargs kill $argv
+  set -l selected_pids (ps -eo pid,args,%mem,%cpu | fzf --multi --header-lines=1 | awk '{print $1}')
+  if test -n "$selected_pids"
+    echo $selected_pids | xargs kill $argv
+  end
 end
